437da8becf82df9c2d1c3ca616e1680678a2eae0
Andrei Masilevich <a.masilevich@yandex.ru>
2022-06-24 09:52:11 +0300

    Up SSL-helpers, fixed tests name

--- tests/main.cpp
+++ tests/main.cpp
@@ -4,10 +4,6 @@
 
 #ifndef NDEBUG
 #include <boost/test/debug.hpp>
-
-#if defined(SERVER_LIB_PLATFORM_WINDOWS)
-#include <crtdbg.h>
-#endif
 #endif
 
 #include <memory>
@@ -19,11 +15,10 @@ std::unique_ptr<logger_type, void (*)(logger_type*)> _s_logger { nullptr, [](log
 
 boost::unit_test::test_suite* init_unit_test_suite(int, char*[])
 {
+    boost::unit_test::framework::master_test_suite().p_name.value = "Test BarbacoaServer library";
+
 #ifndef NDEBUG
     boost::debug::detect_memory_leaks(true);
-#if defined(SERVER_LIB_PLATFORM_WINDOWS)
-    // _CrtSetBreakAlloc(6968);
-#endif
 #endif
     _s_logger.reset(logger_type::get_instance());
     _s_logger->init_debug_log(false, true);
